f83babae27b98889d37cd86f887660852820aa85
giacomo9999 <gmarchesi888@gmail.com>
2020-07-25 14:58:45 -0400

    Add testing for counterReducer.js.

--- src/store/reducers/counterReducer.test.js
+++ src/store/reducers/counterReducer.test.js
@@ -1,1 +1,27 @@
-test("Renders without error", () => {});
+import * as actionTypes from "../actions/actions";
+import reducer from "./counterReducer";
+
+test("Returns default initialState when no value is passed", () => {
+  const newState = reducer(undefined, {});
+  expect(newState).toEqual({ counterValue: 0 });
+});
+
+test("Increments counter when receiving an action of type 'INCREMENT'", () => {
+  const newState = reducer(undefined, { type: actionTypes.INCREMENT });
+  expect(newState).toEqual({ counterValue: 1 });
+});
+
+test("Decrements counter when receiving an action of type 'DECREMENT'", () => {
+  const newState = reducer(undefined, { type: actionTypes.DECREMENT });
+  expect(newState).toEqual({ counterValue: -1 });
+});
+
+test("Adds payload.value to counter when receiving an action of type 'ADD'", () => {
+  const newState = reducer(undefined, { type: actionTypes.ADD, payload: 5 });
+  expect(newState).toEqual({ counterValue: 5 });
+});
+
+test("Subtracts payload.value from counter when receiving an action of type 'SUBTRACT'", () => {
+  const newState = reducer(undefined, { type: actionTypes.SUBTRACT, payload: 5 });
+  expect(newState).toEqual({ counterValue: -5 });
+});
